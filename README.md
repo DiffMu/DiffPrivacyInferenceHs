2b2a5c0331c68e6a63d7c0d853b3689b28562e6d
Maxim Urschumzew <m.urschumzew@outlook.com>
2021-04-11 17:30:59 +0200

    Describe folder structure.

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # DiffPrivacyInferenceHs
+
+## Developer Guide
+### Folder structure
+The subfolders are organized as follows:
+```
+- app  (main entry point into application)
+- src  (actual source code)
+  \- DiffMu
+     |- Prelude    (Common imports from libraries)
+     |- Abstract   (Structures/Code useful for building typecheckers,
+     |              should not contain references to the actual duet type system)
+     |- Core       (Basic definitions of the duet type system, operations on contexts, etc.)
+     |- Typecheck  (Implementation of the actual typechecking and subtyping rules)
+```
+
+
+
--- src/DiffMu/Typecheck/Typecheck.hs
+++ src/DiffMu/Typecheck/Typecheck.hs
@@ -53,10 +53,7 @@ checkSens (Sng η τ) scope  = Numeric <$> Const (constCoeff (Fin η)) <$> creat
 -- a special term for function argument variables.
 -- those get sensitivity 1, all other variables are var terms
 checkSens (Arg x dτ) scope = do τ <- createDMType dτ
-                                setVar x (τ :@ constCoeff (Fin 1)) --(Fin 1))
-                                -- tt <- use types
-                                -- return (traceShow tt τ)
-
+                                setVar x (τ :@ constCoeff (Fin 1))
                                 return τ
 
 checkSens (Var x dτ) scope = do -- get the term that corresponds to this variable from the scope dict
