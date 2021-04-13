e0d5b7ddca71f3a6ae21d266cfd3592508e3c8c2
ooovi <githooovi@posteo.de>
2021-04-13 18:50:46 +0200

    terms, too

--- app/Example/Terms.hs
+++ app/Example/Terms.hs
@@ -16,7 +16,7 @@ t₄ = Op (IsBinary DMOpAdd) [t₃ "x", t₃ "x"]
 
 t₅ a b = Op (IsBinary DMOpAdd) [a, b]
 
+t₆ = Lam (Lam_ [Symbol "y" :- JTAny] (SLet (Symbol "x" :- JTAny) (Sng 3 JTNumInt) (Op (IsBinary DMOpMul) [t₁, Var (Symbol "y") JTAny])))
+
 t₇ = Lam (Lam_ [Symbol "x" :- (JTNum JTNumInt), Symbol "y" :- JTAny] (t₅ t₄ t₂))
-
-
 
