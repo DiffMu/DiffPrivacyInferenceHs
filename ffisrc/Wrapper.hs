34ff12b255d8c4bb2778a73fb87f94a7db07c58e
Maxim Urschumzew <m.urschumzew@outlook.com>
2021-06-29 23:54:27 +0200

    [WIP] Adding hspec testing.
    (To be called from the julia runtime. This begins the work on #11.)

--- ffisrc/Wrapper.hs
+++ ffisrc/Wrapper.hs
@@ -27,63 +27,10 @@ import DiffMu.Runner
 import DiffMu.Core.Definitions
 import DiffMu.Typecheck.JuliaType
 
+import Spec
+
 foreign import ccall "dynamic" mkFun :: FunPtr (CInt -> CInt) -> (CInt -> CInt)
 
--- data SubComplicated = SubComplicated Int Float String deriving(Show, Generic, NFData)
-
--- data Complicated = Complicated {
---   _f1 :: SubComplicated,
---   _f2 :: [Float],
---   _f3 :: Int32 -> Int32
--- }
-
--- instance Show Complicated where
---   show (Complicated f1' f2' f3') = "Complicated:\n\t" ++ show f1' ++ "\n\t" ++ show f2' ++ "\n\tf3(0)=" ++ show (f3' 0)
-
-
--- newComplicated :: Complicated
--- newComplicated = Complicated {
---   _f1 = SubComplicated 5 0.5 "hi",
---   _f2 = [0.1,0.2,5],
---   _f3 = (+2)
--- }
-
--- makeLenses ''Complicated
-
-
--- basicWrapper :: IO ()
--- basicWrapper = print "wrapper" >> run
-
--- getComplicated :: IO (StablePtr Complicated)
--- getComplicated = newStablePtr newComplicated
-
--- printComplicated :: StablePtr Complicated -> IO ()
--- printComplicated ptr = do
---   v <- deRefStablePtr ptr
---   print v
-
--- mutateComplicated :: CFloat -> StablePtr Complicated -> IO (StablePtr Complicated)
--- mutateComplicated (CFloat addme) ptr = do
---   v <- deRefStablePtr ptr
---   let
---     newcomp = over f2 (++[addme]) v
---   freeStablePtr ptr
---   newStablePtr newcomp
-
-
-
--- setAdder :: FunPtr (CInt -> CInt) -> StablePtr Complicated -> IO (StablePtr Complicated)
--- setAdder fptr ptr = do
---   v <- deRefStablePtr ptr
---   let
---     newAdder x = r where
---       CInt r = (mkFun fptr) (CInt x)
---     newcomp = set f3 newAdder v
---   freeStablePtr ptr
---   newStablePtr newcomp
-
--- freeComplicated :: StablePtr Complicated -> IO ()
--- freeComplicated = freeStablePtr
 
 
 callWithCString :: FunPtr (CString -> CString -> Bool) -> String -> String -> Bool
@@ -91,45 +38,23 @@ callWithCString f a b = unsafeLocalState $ do
   withCString a (\ca -> withCString b (\cb -> return $ call_StringStringBool f ca cb))
 
 
-
 typecheckFromCString_DMTerm :: FunPtr (CString -> CString -> Bool) -> CString -> IO ()
 typecheckFromCString_DMTerm fun str = do
   putStrLn "hello!"
-  -- let ident = "Integer"
-  -- int <- (newCString ident)
-  -- let int = (JuliaType ident)
-
-  -- let ident = "Real"
-  -- real <- (newCString ident)
-  -- -- let real = (JuliaType ident cident)
-  -- putStrLn $ "I want to call: " <> show fun <> "with " <> show int <> " and " <> show real
-  -- -- let myres = callWithCString fun "Integer" "Real"
-  -- let a = "Integer"
-  -- let b = "Real"
-  -- let myres2 = unsafeLocalState (withCString a (\ca -> withCString b (\cb -> return $ call_StringStringBool fun ca cb)))
-  -- putStrLn $ "myres is" <> show myres2
-
 
   writeIORef global_callback_issubtype (makeDMEnv (fun))
   str' <- peekCString str
   typecheckFromString_DMTerm str'
 
-  -- putStrLn $ "I got the string: {" <> str' <> "}"
-
-
--- foreign export ccall basicWrapper :: IO ()
--- foreign export ccall getComplicated :: IO (StablePtr Complicated)
--- foreign export ccall printComplicated :: StablePtr Complicated -> IO ()
--- foreign export ccall freeComplicated :: StablePtr Complicated -> IO ()
--- foreign export ccall mutateComplicated :: CFloat -> StablePtr Complicated -> IO (StablePtr Complicated)
--- foreign export ccall setAdder :: FunPtr (CInt -> CInt) -> StablePtr Complicated -> IO (StablePtr Complicated)
-
 foreign export ccall typecheckFromCString_DMTerm :: FunPtr (CString -> CString -> Bool) -> CString -> IO ()
--- foreign export ccall typecheckFromCString_DMTerm :: FunPtr ((() -> IO CString) -> (() -> IO CString) -> Bool) -> CString -> IO ()
 
--- foreign import ccall "dynamic" call_StringStringBool :: FunPtr (CString -> CString -> Bool) -> CString -> CString -> Bool
 
--- foreign import ccall "dynamic" call_StringStringBool :: FunPtr (FunPtr (StablePtr String -> IO CString) -> StablePtr String -> StablePtr String -> Bool) -> FunPtr (StablePtr String -> IO CString) -> StablePtr String -> StablePtr String -> Bool
--- foreign import ccall "dynamic" call_StringIOStringIOBool :: FunPtr (FunPtr (StablePtr String -> IO CString) -> StablePtr String -> StablePtr String -> Bool) -> FunPtr (StablePtr String -> IO CString) -> StablePtr String -> StablePtr String -> Bool
+runHaskellTests :: FunPtr (CString -> CString -> Bool) -> IO ()
+runHaskellTests fun = do
+  putStrLn "We are testing now!"
+  writeIORef global_callback_issubtype (makeDMEnv (fun))
+  runAllTests
+
+foreign export ccall runHaskellTests :: FunPtr (CString -> CString -> Bool) -> IO ()
 
 
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,39 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+
+module Spec where
+
+import DiffMu.Prelude
+import DiffMu.Abstract
+import DiffMu.Core
+import DiffMu.Core.TC
+import DiffMu.Core.Definitions
+import DiffMu.Core.Symbolic
+import DiffMu.Core.Context
+import DiffMu.Typecheck.Operations
+import DiffMu.Typecheck.Subtyping
+import DiffMu.Typecheck.Typecheck
+import DiffMu.Parser.DMTerm.FromString
+
+import DiffMu.Typecheck.JuliaType
+
+import Algebra.PartialOrd
+
+import           Foreign.C.String
+
+import Debug.Trace
+
+
+import Test.Hspec
+import Test.QuickCheck
+
+
+runAllTests :: IO ()
+runAllTests = hspec $ do
+  describe "Prelude.head" $ do
+    it "returns the first element of a list" $ do
+      head [23 ..] `shouldBe` (23 :: Int)
+
+
+
+
+
+
