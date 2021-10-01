09db623d7c889f31f3c02f77de32caf16dbb6579
Maxim Urschumzew <m.urschumzew@outlook.com>
2021-10-01 17:42:56 +0200

    Partially parsing of builtins.

--- src/DiffMu/Parser/Expr/JExprToDMTerm.hs
+++ src/DiffMu/Parser/Expr/JExprToDMTerm.hs
@@ -5,8 +5,14 @@ import DiffMu.Prelude
 --import DiffMu.Abstract
 import DiffMu.Core
 import DiffMu.Parser.Expr.FromString
+import qualified Data.Text as T
 
 type ParseState = (StateT (String,Int) (Except DMException))
+
+parseError :: String -> ParseState a
+parseError message = do
+  (file,line) <- get
+  throwError (ParseError message file line)
 
 pList :: [JExpr] -> ParseState ParseDMTerm
 pList [] = error "bla"
@@ -26,6 +32,25 @@ pSingle e = case e of
                  JEInteger n -> pure $ Sng n (JuliaType "Integer")
                  JEReal r -> pure $ Sng r (JuliaType "Real")
                  JEReturn ret -> pSingle ret
+
+
+pCall :: JExpr -> [JExpr] -> ParseState ParseDMTerm
+-- if the term is a symbol, we check if it
+-- is a builtin/op, if so, we construct the appropriate DMTerm
+pCall (JESymbol (Symbol sym)) args = case (sym,args) of
+
+  (t@":gaussian_mechanism!", [a1, a2, a3, a4]) -> Gauss <$> pSingle a1 <*> pSingle a2 <*> pSingle a3 <*> pSingle a4
+  (t@":gaussian_mechanism!", args) -> parseError $ "The builtin '" <> T.unpack t <> "' requires 4 arguments, but has been given " <> show (length args)
+
+  (t@":subtract_gradient!", [a1, a2]) -> SubGrad <$> pSingle a1 <*> pSingle a2
+  (t@":subtract_gradient!", args) -> parseError $ "The builtin '" <> T.unpack t <> "' requires 2 arguments, but has been given " <> show (length args)
+
+  (t@":clip!", [a1,a2]) -> ClipM <$> undefined <*> pSingle a2
+  (t@":clip!", args) -> parseError $ "The builtin '" <> T.unpack t <> "' requires 2 arguments, but has been given " <> show (length args)
+
+  _ -> undefined
+
+pCall term args = throwError (UnsupportedError $ "Term " <> show term <> " currently not supported")
 
 
                  -- JECall JExpr [JExpr]
