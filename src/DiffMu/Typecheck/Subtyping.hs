8b484bd14853d3f612b8806c218b97665888fd13
ooovi <githooovi@posteo.de>
2021-11-01 16:30:57 +0100

    adding subtyping and unification for vectors

--- src/DiffMu/Typecheck/Subtyping.hs
+++ src/DiffMu/Typecheck/Subtyping.hs
@@ -121,12 +121,22 @@ subtypingGraph =
                       a₁ <- newVar
                       a₀ ⊑! a₁
                       return ((DMMat nrm clp n m a₀), (DMMat nrm clp n m a₁))
+                 , SingleEdge $
+                   do nrm <- newVar
+                      clp <- newVar
+                      n <- newVar
+                      a₀ <- newVar
+                      a₁ <- newVar
+                      a₀ ⊑! a₁
+                      return ((DMVec nrm clp n a₀), (DMVec nrm clp n a₁))
+                      {-
                  , SingleEdge $ -- this is the conv rule made implicit, for converting DMData to DMReal
                    do nrm <- newVar
                       clp <- newVar
                       n <- newVar
                       m <- newVar
                       return ((DMMat nrm (Clip clp) n m (Numeric DMData)), (DMMat clp U n m (Numeric (NonConst DMReal))))
+                    -}
                  , SingleEdge $
                    do m <- newVar
                       a₀ <- newVar
@@ -141,11 +151,13 @@ subtypingGraph =
                       a₁ <- newVar
                       a₀ ⊑! a₁
                       return ((DMGrads nrm clp m a₀), (DMGrads nrm clp m a₁))
+                 {-
                  , SingleEdge $ -- this is the conv rule made implicit, for converting DMData to DMReal
                    do nrm <- newVar
                       clp <- newVar
                       m <- newVar
                       return ((DMGrads nrm (Clip clp) m (Numeric DMData)), (DMGrads clp U m (Numeric (NonConst DMReal))))
+-}
                  ]
             ; _ -> []
             }
