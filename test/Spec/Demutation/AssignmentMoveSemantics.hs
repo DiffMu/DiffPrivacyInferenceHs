623c66628923b035962af3577248d07bbafeeb41
Maxim Urschumzew <m.urschumzew@outlook.com>
2022-04-24 16:41:25 +0200

    Fix tests after changing how if branches are merged.

--- test/Spec/Demutation/AssignmentMoveSemantics.hs
+++ test/Spec/Demutation/AssignmentMoveSemantics.hs
@@ -138,6 +138,17 @@ testAMS04 pp = do
             \  else                \n\
             \    y = a             \n\
             \  end                 \n\
+            \  internal_mutate!(y) \n\
+            \  return              \n\
+            \end                   "
+
+  let exb_pass = " function test(a,b,c) \n\
+            \  y = 0               \n\
+            \  if c                \n\
+            \    y = a             \n\
+            \  else                \n\
+            \    y = b             \n\
+            \  end                 \n\
             \  clone(y)            \n\
             \end                   "
 
@@ -149,7 +160,8 @@ testAMS04 pp = do
             \  else                \n\
             \    y = b             \n\
             \  end                 \n\
-            \  clone(y)            \n\
+            \  internal_mutate!(y) \n\
+            \  return              \n\
             \end                   "
 
 
@@ -168,9 +180,11 @@ testAMS04 pp = do
       bool = NoFun(DMBool)
       intnc' = (Numeric (Num (IRNum DMInt) NonConst))
 
-      tya = Fun([([intnc :@ (constCoeff $ Fin 2), intnc :@ (zeroId), bool :@ (constCoeff $ Infty)] :->: (NoFun $ intnc')) :@ Just [JTAny, JTAny, JTAny]])
+      tya = Fun([([intnc :@ (constCoeff $ Fin 4), intnc :@ (zeroId), bool :@ (constCoeff $ Infty)] :->: (NoFun $ intnc')) :@ Just [JTAny, JTAny, JTAny]])
+      tyb_pass = Fun([([intnc :@ (constCoeff $ Fin 1), intnc :@ (constCoeff $ Fin 1), bool :@ (constCoeff $ Infty)] :->: (NoFun $ intnc')) :@ Just [JTAny, JTAny, JTAny]])
 
   parseEvalUnify pp "same move in the branches is allowed" exa (pure tya)
-  parseEvalFail pp "different moves in the branches is not allowed" exb (DemutationError "")
+  parseEvalUnify pp "different moves in the branches is allowed if only used in clone" exb_pass (pure tyb_pass)
+  parseEvalFail pp "different moves in the branches is not allowed if used in function call" exb (DemutationError "")
   parseEvalFail pp "mutation after different moves in the branches is not allowed" exc (DemutationError "")
   return ()
